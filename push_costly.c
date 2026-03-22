bbbd4bf6b96e5e008236bae1ac88d8dd4ab5042b
Lathikaa J <130064380+lathikaa-j@users.noreply.github.com>
2026-03-22 22:20:37 +0530

    Implementation of stack using two queues woth push costly

---
+++ push_costly.c
@@ -0,0 +1,71 @@
+#include<stdio.h>
+#define MAX 50
+int q1[MAX], q2[MAX];
+int f1=0, r1=-1;
+int f2=0, r2=-1;
+void enqueue1(int x)
+{
+    r1++;
+    q1[r1] = x;
+}
+void enqueue2(int x)
+{
+    r2++;
+    q2[r2] = x;
+}
+int dequeue1()
+{
+    int x = q1[f1];
+    f1++;
+    return x;
+}
+int dequeue2()
+{
+    int x = q2[f2];
+    f2++;
+    return x;
+}
+void push(int x)
+{
+    enqueue2(x);
+    while(f1<=r1)
+        enqueue2(dequeue1());
+    f1=0;
+    r1=-1;
+    for(int i = f2; i<=r2;i++)
+        q1[++r1] = q2[i];
+    f2=0;
+    r2=-1;
+}
+int pop()
+{
+    if(f1>r1)
+        return -1;
+    return dequeue1();
+}
+int main()
+{
+    push(34);
+    push(56);
+    push(19);
+    push(87);
+    printf("Popped element is %d\n", pop());
+    printf("Popped element is %d\n", pop());
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
